56659f58370d1013b296a4d7289119871484c9fd
Vinícius Martins <vinicius.pereira.mts@gmail.com>
2023-10-11 22:50:19 +0000

    Ajustes Requisicao API

--- src/api/simple_api.py
+++ src/api/simple_api.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "API Rodando"
+    return "Basic API is running"
 
 @app.route("/api", methods=['POST'])
 def api():
--- src/app/server.js
+++ src/app/server.js
@@ -5,11 +5,22 @@ const app = new express()
 
 app.use(express.static(path.join(__dirname, "/public")));
 
-const url = "http://ml-api:5000"; // substitua por sua URL da API
-
-app.get('/api/data', async (req, res) => {
+app.get('/api', async (req, res) => {
+    const url = "http://ml-api:5000";
     try {
         const response = await axios.get(url);
+        res.send(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Ocorreu um erro ao buscar os dados da API.');
+    }
+});
+
+app.post('/api/data', async (req, res) => {
+    const url = "http://ml-api:5000/api";
+    const data = req.body; // Dados recebidos na requisição POST
+    try {
+        const response = await axios.post(url, data); // Passa os dados para a função axios.post
         res.send(response.data);
     } catch (error) {
         console.error(error);
