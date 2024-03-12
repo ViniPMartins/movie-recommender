c57221a32409afb563068ce0eb9f7b6011b8bb10
ViniPMartins <vinicius.pereira.mts@gmail.com>
2024-03-11 22:41:11 -0300

    Adjust API

--- src/api/main.py
+++ src/api/main.py
@@ -4,6 +4,10 @@ from scripts import pipeline_data
 import pandas as pd
 import pickle
 import os
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # pipeline = pickle.load(open('models/pipeline.sav', 'rb'))
 kmeans = pickle.load(open('models/kmeans_algorithm.sav', 'rb'))
@@ -23,14 +27,17 @@ def home():
 @app.route('/api/recommender_movie/', methods=['POST'])
 @basic_auth.required
 def recommender_movie():
-    params = request.get_json()
+    params = json.loads(request.get_json())
     generos_pipe = pipeline_data(params['generos'])
     group_predict = kmeans.predict(generos_pipe)
+
+    print("n_min_aval", params['n_min_aval'], type(params['n_min_aval']))
+    print("rating_min_aval", params['rating_min_aval'], type(params['rating_min_aval']))
 
     movies_recomended = movies_ratings.query(
         f'group=={group_predict} and n_rating>{params["n_min_aval"]} and mean_rating>{params["rating_min_aval"]}'
         ).sort_values(by='mean_rating', ascending=False).reset_index(drop=True)
-    
+
     return movies_recomended.to_json()
 
 if '__main__' == __name__:
