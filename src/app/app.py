30e35fa48d70103ba53a4783721d00fce97534e7
ViniPMartins <vinicius.pereira.mts@gmail.com>
2024-03-11 22:40:21 -0300

    Fixing and ajusts in app

--- src/app/app.py
+++ src/app/app.py
@@ -3,17 +3,23 @@ import requests
 import json
 import os
 import pandas as pd
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_movies(data):
     data_json = json.dumps(data)
-    data_auth = {
-        "username": os.environ.get("BASIC_AUTH_USERNAME"),
-        "password": os.environ.get("BASIC_AUTH_PASSWORD")
-    }
-    url = os.environ.get("API_URL") + ":" + os.environ.get("API_PORT")
-    response = requests.post(url, data=data_json, auth=data_auth)
-    df_movies = pd.read_json(response)
+    data_auth =(os.environ.get("BASIC_AUTH_USERNAME"),os.environ.get("BASIC_AUTH_PASSWORD"))
+    url = os.environ.get("API_URL") + ":" + os.environ.get("API_PORT") + os.environ.get("API_PATH")
+    response = requests.post(url, json=data_json, auth=data_auth)
+    df_movies = pd.read_json(response.text)
     return df_movies
+
+st.set_page_config(
+   page_title="Movie Recommender",
+   page_icon="🎞️",
+   layout="wide",
+)
 
 st.title("RECOMMENDING MOVIES WITH AI")
 
@@ -23,7 +29,7 @@ recommends movies according to your choice. You can select the genre of the movi
 the minimum average rating, and the minimum number of reviews the movie can have received.
 ''')
 
-all_genres = ['(no genres listed)', 'Action', 'Adventure', 'Animation', 'Children',
+all_genres = ['Action', 'Adventure', 'Animation', 'Children',
         'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy', 'Film-Noir',
         'Horror', 'IMAX', 'Musical', 'Mystery', 'Romance', 'Sci-Fi', 'Thriller',
         'War', 'Western']
@@ -34,11 +40,14 @@ min_n_reviews = st.slider('Minimum Number of Reviews', 10, 100, 55, 1)
 
 data = {
     "generos":selected_genres,
-    "n_min_aval":min_average_rat,
-    "rating_min_aval":min_n_reviews
+    "rating_min_aval":min_average_rat,
+    "n_min_aval":min_n_reviews
 }
 
-with st.spinner("Getting data..."):
-    generate_movies = st.button("Generate recommendations", type='primary', on_click=get_movies(data))
-
-st.dataframe(generate_movies)
+if st.button("Generate recommendations", type="primary"):
+    with st.spinner("Getting data..."):
+        try:
+            generate_movies = get_movies(data)
+            st.dataframe(generate_movies, use_container_width=True)
+        except:
+            st.error("Unable to obtain data")
